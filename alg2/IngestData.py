bb6318c8069777beb300d68c0b7e865842bd0fb3
theinfamousmrmeow <alexander.d.macey@gmail.com>
2020-12-04 09:52:45 -0500

    Ingesting data

--- alg2/IngestData.py
+++ alg2/IngestData.py
@@ -1,10 +1,28 @@
 import csv
 from alg2.HashTable import HashTable
 
-with open('WGUInputData.csv') as csv:
-    slurpCSV = csv.reader(csv, delimeter=",")
+with open('WGUPS_PACKAGE_FILE.csv') as input_file:
+    slurpCSV = csv.reader(input_file, delimiter=",")
     working_hash_table = HashTable() #Make a new hashtable to work with
     truck_one = []
     truck_two = []
     truck_three = [] #Keep in mind this is actually the first truck, just making a second run
 
+    for r in slurpCSV:
+        r_package_ID=r[0]
+        r_address = r[1]
+        r_city = r[2]
+        r_state = r[3]
+        r_zip = r[4]
+        r_delivery = r[5]
+        r_size = r[6]
+        r_note = r[7]
+        r_value = [r_package_ID, "", r_address, r_city, r_state, r_zip, r_delivery, r_size, r_note, "", "At hub"]
+        key = r_package_ID
+
+        #TODO: Perform hard checks to clean up data before processing
+
+        working_hash_table.insert(key,r_value)
+
+    def get_hash_map():
+        return working_hash_table
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #IMPORTS
 
 from alg2.HashTable import HashTable
+from alg2.IngestData import get_hash_map
 
 if __name__ == '__main__':
 
@@ -10,17 +11,20 @@ if __name__ == '__main__':
     ht.print()
     ht.insert(0,"what a cat does")
     ht.print()
-
+    get_hash_map().print()
     commandList = '\tfind - Find a package at a given time.\n\ttime - find package states at a certain time.\n\tquit - End program.\n'
     command = "none"
 
     while command is not 'quit':
         if command == 'find':
-            ##Do lookup routine
-            lookup_id = input('Please give package id:\n')
-            lookup_time = input('At what time?  HH:MM:SS:\n')
-            ###Print package data from Htable
-            command = ""
+            try:
+                ##Do lookup routine
+                lookup_id = input('Please give package id:\n')
+                lookup_time = input('At what time?  HH:MM:SS:\n')
+                ###Print package data from Htable
+                command = ""
+            except ValueError:
+                print("Error happened, try again.")
         elif command == 'quit':
             ##Quit
             exit()
